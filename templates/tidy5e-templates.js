60c9f28482230ea98648cb7124b91a5659fdc027
sdenec <hallo@stefandenecke.de>
2020-06-20 15:07:59 +0200

    more comprimated skills, feats and attributes

--- templates/tidy5e-templates.js
+++ templates/tidy5e-templates.js
@@ -11,6 +11,7 @@ export const preloadTidy5eHandlebarsTemplates = async function() {
 
     // Actor Sheet Partials
     "modules/tidy5e-sheet/templates/parts/tidy5e-traits.html",
+    "modules/tidy5e-sheet/templates/parts/tidy5e-traits-small.html",
     "modules/tidy5e-sheet/templates/parts/tidy5e-inventory.html",
     "modules/tidy5e-sheet/templates/parts/tidy5e-features.html",
     "modules/tidy5e-sheet/templates/parts/tidy5e-spellbook.html"
