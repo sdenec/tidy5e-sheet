fd26f74eba4d29342f5d243bb33a97b1da4c741f
sdenec <hallo@stefandenecke.de>
2020-06-22 15:30:15 +0200

    template source fix

--- templates/tidy5e-npc-templates.js
+++ templates/tidy5e-npc-templates.js
@@ -10,9 +10,9 @@ export const preloadTidy5eHandlebarsTemplates = async function() {
   const tidy5etemplatePaths = [
 
     // Actor Sheet Partials
-    "modules/tidy5e-npc/templates/npc/parts/tidy5e-npc-traits.html",
-    "modules/tidy5e-npc/templates/npc/parts/tidy5e-npc-features.html",
-    "modules/tidy5e-npc/templates/npc/parts/tidy5e-npc-spellbook.html"
+    "modules/tidy5e-sheet/templates/npc/parts/tidy5e-npc-traits.html",
+    "modules/tidy5e-sheet/templates/npc/parts/tidy5e-npc-features.html",
+    "modules/tidy5e-sheet/templates/npc/parts/tidy5e-npc-spellbook.html"
   ];
 
   // Load the template parts
