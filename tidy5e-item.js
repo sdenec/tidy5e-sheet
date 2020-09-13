3d95f44ee156f8c4689240cd4002934577e40caa
akrigline <a.krigline@gmail.com>
2020-09-13 11:51:50 -0400

    register tidy item sheet with better rolls

--- tidy5e-item.js
+++ tidy5e-item.js
@@ -10,3 +10,9 @@ export class Tidy5eItemSheet extends ItemSheet5e {
 
 // Register Tidy5e Item Sheet and make default
 Items.registerSheet("dnd5e", Tidy5eItemSheet, {makeDefault: true});
+
+Hooks.once("ready", () => {
+  if (window.BetterRolls) {
+    window.BetterRolls.hooks.addItemSheet("Tidy5eItemSheet");
+	}
+});
