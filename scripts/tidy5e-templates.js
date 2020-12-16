103bd7a4d62408ad167c5cca0c10038346cc65d1
sdenec <hallo@stefandenecke.de>
2020-12-16 22:02:39 +0100

    0.4.0

--- scripts/tidy5e-templates.js
+++ scripts/tidy5e-templates.js
@@ -13,9 +13,13 @@ export const preloadTidy5eHandlebarsTemplates = async function() {
     "modules/tidy5e-sheet/templates/actors/parts/tidy5e-traits.html",
     "modules/tidy5e-sheet/templates/actors/parts/tidy5e-inventory.html",
     "modules/tidy5e-sheet/templates/actors/parts/tidy5e-inventory-grid.html",
+    "modules/tidy5e-sheet/templates/actors/parts/tidy5e-inventory-header.html",
+    "modules/tidy5e-sheet/templates/actors/parts/tidy5e-inventory-footer.html",
     "modules/tidy5e-sheet/templates/actors/parts/tidy5e-features.html",
     "modules/tidy5e-sheet/templates/actors/parts/tidy5e-spellbook.html",
     "modules/tidy5e-sheet/templates/actors/parts/tidy5e-spellbook-grid.html",
+    "modules/tidy5e-sheet/templates/actors/parts/tidy5e-spellbook-header.html",
+    "modules/tidy5e-sheet/templates/actors/parts/tidy5e-spellbook-footer.html",
     "modules/tidy5e-sheet/templates/actors/parts/tidy5e-effects.html"
   ];
 
