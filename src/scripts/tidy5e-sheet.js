867d784a28c7400a4b33fdee529b2c33beca8db0
flamewave000 <3588046+flamewave000@users.noreply.github.com>
2021-06-21 18:42:23 -0400

    Add selector for switching ammo on ranged weapons

--- src/scripts/tidy5e-sheet.js
+++ src/scripts/tidy5e-sheet.js
@@ -66,11 +66,45 @@ export class Tidy5eSheet extends ActorSheet5eCharacter {
 		
 		let actor = this.actor;
 
-    tidy5eListeners(html, actor);
-    tidy5eContextMenu(html);
+		tidy5eListeners(html, actor);
+		tidy5eContextMenu(html);
 		tidy5eSearchFilter(html, actor);
 		tidy5eShowActorArt(html, actor);
 		tidy5eItemCard(html, actor);
+
+		html.find('.ammo').each(function () {
+			const element = $(this);
+			const itemId = element.attr('data-id');
+			const actor = game.actors.find(x => x.items.some(b => b.id === itemId));
+			const item = actor.items.get(itemId);
+			const equippedOnly = game.settings.get('tidy5e-sheet', 'ammoEquippedOnly');
+			const ammoItems = actor.items.filter(x => x.data.data.consumableType === "ammo" && (!equippedOnly || x.data.data.equipped));
+			const target = item.data.data.consume.target;
+			const ammoItemStrings = ['<option value=""></option>']
+				.concat(ammoItems.map(x => `<option value="${x.id}" ${x.id === target ? 'selected' : ''}>${x.name}</option>`))
+				.join('');
+			const selector = $(`<select style="height:unset;margin-left:0.25em;">${ammoItemStrings}</select>`);
+			selector.attr('data-item', item.id);
+			selector.attr('data-actor', actor.id);
+			selector.on('change', function () {
+				const element = $(this);
+				const val = element.val();
+				const actor = game.actors.get(selector.attr('data-actor'));
+				const item = actor.items.get(selector.attr('data-item'));
+				const ammo = actor.items.get(val);
+				item.update({
+					data: {
+						consume: {
+							amount: !ammo ? null : !!item.data.data.consume.amount ? item.data.data.consume.amount : 1,
+							target: !ammo ? '' : val,
+							type: !ammo ? '' : ammo.data.data.consumableType
+						}
+					}
+				});
+			});
+			element.after(selector);
+			element.remove();
+		});
 
 		// store Scroll Pos
 		const attributesTab = html.find('.tab.attributes');
