015bb4810d576af0eb5ea6e567c2355bf5efa744
p4535992 <tentimarco0@gmail.com>
2022-12-23 23:11:18 +0100

    testing fix

--- src/scripts/app/lazyExpAndHp.js
+++ src/scripts/app/lazyExpAndHp.js
@@ -128,6 +128,68 @@ function _onChangeHp(ev) {
     
 }
 
+function _onChangeHpMax(ev) {
+    const input = ev.target;
+    const actor = ev.data.app.actor;
+    const sheet = ev.data.app.options;
+    const hp = ev.data.app.actor.system.attributes.hp.value;
+    const maxHp = ev.data.app.actor.system.attributes.hp.max;
+    const minHp = ev.data.app.actor.system.attributes.hp.min;
+    const denom = input.name.split(".")[2];
+    const value = input.value;
+    let sign = signCase.default;
+    Object.values(signCase).forEach((val) => {
+        if (value.includes(val)) {
+            sign = val;
+        }
+    });
+    const splitVal = value.split(sign);
+    let delta;
+    if (splitVal.length > 1) {
+        delta = Number(splitVal[1]);
+    }
+    else {
+        delta = Number(splitVal[0]);
+    }
+    let newAmount = {};
+
+    switch (sign) {
+        case signCase.add: {
+            newAmount = Number(maxHp) + delta;
+            break;
+        }
+        case signCase.subtract: {
+            newAmount = Number(maxHp) - delta;
+            break;
+        }
+        case signCase.equals: {
+            newAmount = delta;
+            break;
+        }
+        default: {
+            newAmount = delta;
+            break;
+        }
+    }
+
+    // if(newAmount > maxHp) {
+    //     newAmount = maxHp;
+    // }
+    if(newAmount <  minHp) {
+        newAmount = minHp;
+    }
+    
+    sheet.submitOnChange = false;
+    actor
+        .update({ "system.attributes.hp.max": newAmount })
+        .then(() => {
+        input.value = getProperty(actor.data, input.name);
+        sheet.submitOnChange = true;
+    })
+    .catch(console.log.bind(console));
+    
+}
+
 export function applyLazyExp(app, html, actorData) {
     // if (!game.settings.get('tidy5e-sheet', "lazyExpEnable")) {
     //     return;
@@ -156,4 +218,16 @@ export function applyLazyHp(app, html, actorData) {
         app: app,
         data: actorData,
     }, _onChangeHp);
+
+    for (const elem of html.find("input[name^='system.attributes.hp.max']")) {
+        elem.type = "text";
+        elem.classList.add("lazyhp");
+    }
+    html.find("input[name^='system.attributes.hp.max']").off("change");
+    html.find("input[name^='system.attributes.hp.max']").change({
+        app: app,
+        data: actorData,
+    }, _onChangeHpMax);
+
+    
 }
