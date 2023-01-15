11e18ef3ed1d8e66a8ab3e6114a76dc5d346cd41
p4535992 <tentimarco0@gmail.com>
2023-01-15 22:22:53 +0100

    update migrationutil

--- src/scripts/app/migration-util.js
+++ src/scripts/app/migration-util.js
@@ -1,88 +1,153 @@
+
 export function migrateFor21X(app, html, data){
-    if(!isEmptyObject(app.object?.system?.details?.notes)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
+    return migrateActorFor21X(app.object);
+}
+export function migrateActorFor21X(actor){
+    if(!isEmptyObject(actor?.system?.details?.notes)) {
+        if(!actor.flags) {
+            actor.flags = {};
         }
-        if(isEmptyObject(app.object.flags["tidy5e-sheet"]?.notes)) {
-            app.object.setFlag("tidy5e-sheet", "notes", app.object?.system.details.notes);
-        }
-    }
-
-    if(!isEmptyObject(app.object?.system?.details?.notes1)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
-        }
-        if(isEmptyObject(app.object.flags["tidy5e-sheet"]?.notes1)) {
-            app.object.setFlag("tidy5e-sheet", "notes1", app.object?.system.details.notes1);
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.notes)) {
+            actor.setFlag("tidy5e-sheet", "notes", actor?.system.details.notes);
         }
     }
 
-    if(!isEmptyObject(app.object?.system?.details?.notes2)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
+    if(!isEmptyObject(actor?.system?.details?.notes1)) {
+        if(!actor.flags) {
+            actor.flags = {};
         }
-        if(isEmptyObject(app.object.flags["tidy5e-sheet"]?.notes2)) {
-            app.object.setFlag("tidy5e-sheet", "notes2", app.object?.system.details.notes2);
-        }
-    }
-
-    if(!isEmptyObject(app.object?.system?.details?.notes3)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
-        }
-        if(isEmptyObject(app.object.flags["tidy5e-sheet"]?.notes3)) {
-            app.object.setFlag("tidy5e-sheet", "notes3", app.object?.system.details.notes3);
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.notes1)) {
+            actor.setFlag("tidy5e-sheet", "notes1", actor?.system.details.notes1);
         }
     }
 
-    if(!isEmptyObject(app.object?.system?.details?.notes4)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
+    if(!isEmptyObject(actor?.system?.details?.notes2)) {
+        if(!actor.flags) {
+            actor.flags = {};
         }
-        if(isEmptyObject(app.object.flags["tidy5e-sheet"]?.notes4)) {
-            app.object.setFlag("tidy5e-sheet", "notes4", app.object?.system.details.notes4);
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.notes2)) {
+            actor.setFlag("tidy5e-sheet", "notes2", actor?.system.details.notes2);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.notes3)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.notes3)) {
+            actor.setFlag("tidy5e-sheet", "notes3", actor?.system.details.notes3);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.notes4)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.notes4)) {
+            actor.setFlag("tidy5e-sheet", "notes4", actor?.system.details.notes4);
         }
     }
 
     // ===================
 
-    if(!isEmptyObject(app.object?.system?.details?.notes1name)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
+    if(!isEmptyObject(actor?.system?.details?.notes1name)) {
+        if(!actor.flags) {
+            actor.flags = {};
         }
-        const notes1 = app.object.getFlag("tidy5e-sheet", "notes1");
-        notes1.name = app.object?.system?.details?.notes1name;
-        app.object.setFlag("tidy5e-sheet", "notes1", notes1);
+        const notes1 = actor.getFlag("tidy5e-sheet", "notes1");
+        notes1.name = actor?.system?.details?.notes1name;
+        actor.setFlag("tidy5e-sheet", "notes1", notes1);
     }
 
-    if(!isEmptyObject(app.object?.system?.details?.notes2name)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
+    if(!isEmptyObject(actor?.system?.details?.notes2name)) {
+        if(!actor.flags) {
+            actor.flags = {};
         }
-        const notes2 = app.object.getFlag("tidy5e-sheet", "notes2");
-        notes2.name = app.object?.system?.details?.notes2name;
-        app.object.setFlag("tidy5e-sheet", "notes2", notes2);
+        const notes2 = actor.getFlag("tidy5e-sheet", "notes2");
+        notes2.name = actor?.system?.details?.notes2name;
+        actor.setFlag("tidy5e-sheet", "notes2", notes2);
     }
 
-    if(!isEmptyObject(app.object?.system?.details?.notes3name)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
+    if(!isEmptyObject(actor?.system?.details?.notes3name)) {
+        if(!actor.flags) {
+            actor.flags = {};
         }
-        const notes3 = app.object.getFlag("tidy5e-sheet", "notes3");
-        notes3.name = app.object?.system?.details?.notes3name;
-        app.object.setFlag("tidy5e-sheet", "notes3", notes3);
+        const notes3 = actor.getFlag("tidy5e-sheet", "notes3");
+        notes3.name = actor?.system?.details?.notes3name;
+        actor.setFlag("tidy5e-sheet", "notes3", notes3);
     }
 
-    if(!isEmptyObject(app.object?.system?.details?.notes4name)) {
-        if(!app.object.flags) {
-            app.object.flags = {};
+    if(!isEmptyObject(actor?.system?.details?.notes4name)) {
+        if(!actor.flags) {
+            actor.flags = {};
         }
-        const notes4 = app.object.getFlag("tidy5e-sheet", "notes4");
-        notes4.name = app.object?.system?.details?.notes4name;
-        app.object.setFlag("tidy5e-sheet", "notes4", notes4);
+        const notes4 = actor.getFlag("tidy5e-sheet", "notes4");
+        notes4.name = actor?.system?.details?.notes4name;
+        actor.setFlag("tidy5e-sheet", "notes4", notes4);
     }
-    
 
+    if(!isEmptyObject(actor?.system?.details?.gender)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.gender)) {
+            actor.setFlag("tidy5e-sheet", "gender", actor?.system.details.gender);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.age)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.age)) {
+            actor.setFlag("tidy5e-sheet", "age", actor?.system.details.age);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.height)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.height)) {
+            actor.setFlag("tidy5e-sheet", "height", actor?.system.details.height);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.weight)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.weight)) {
+            actor.setFlag("tidy5e-sheet", "weight", actor?.system.details.weight);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.eyes)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.eyes)) {
+            actor.setFlag("tidy5e-sheet", "eyes", actor?.system.details.eyes);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.skin)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.skin)) {
+            actor.setFlag("tidy5e-sheet", "skin", actor?.system.details.skin);
+        }
+    }
+
+    if(!isEmptyObject(actor?.system?.details?.hair)) {
+        if(!actor.flags) {
+            actor.flags = {};
+        }
+        if(isEmptyObject(actor.flags["tidy5e-sheet"]?.hair)) {
+            actor.setFlag("tidy5e-sheet", "hair", actor?.system.details.hair);
+        }
+    }
 }
 
 function isEmptyObject(obj) {
