b5d30758dfb5309fe48e7be13c96a1808662c372
Kevin Garner <kevinleegarner@gmail.com>
2023-11-19 11:55:58 -0600

    Updated origina summary config to account for deprecations.
    Removed Source from origin summary config.
    
    Made Race and Background readonly in origin summary config when Race and Background items are assigned to the character, respectively.

--- src/scripts/app/tidy5e-actor-origin-summary-config.js
+++ src/scripts/app/tidy5e-actor-origin-summary-config.js
@@ -40,16 +40,17 @@ export default class Tidy5eActorOriginSummaryConfig extends Tidy5eBaseConfigShee
   /** @inheritdoc */
   getData(options) {
     return {
-      race: this.clone.system.details.race,
-      background: this.clone.system.details.background,
+      race: this.clone.system.details.race?.name ?? this.clone.system.details.race,
+      background: this.clone.system.details.background?.name ?? this.clone.system.details.background,
       environment: this.clone.system.details.environment,
       alignment: this.clone.system.details.alignment,
-      source: this.clone.system.details.source,
       dimensions: this.clone.system.traits.dimensions,
 
       isCharacter: this.document.type === "character",
       isNPC: this.document.type === "npc",
       isVehicle: this.document.type === "vehicle",
+      canEditBackground: !this.clone.system.details.background?.name,
+      canEditRace: !this.clone.system.details.race?.name,
     };
   }
 
@@ -68,7 +69,6 @@ export default class Tidy5eActorOriginSummaryConfig extends Tidy5eBaseConfigShee
     const background = foundry.utils.expandObject(formData).background;
     const environment = foundry.utils.expandObject(formData).environment;
     const alignment = foundry.utils.expandObject(formData).alignment;
-    const source = foundry.utils.expandObject(formData).source;
 
     const dimensions = foundry.utils.expandObject(formData).dimensions;
 
@@ -77,35 +77,17 @@ export default class Tidy5eActorOriginSummaryConfig extends Tidy5eBaseConfigShee
     const isVehicle = this.document.type === "vehicle";
 
     if (isCharacter) {
-      // this.clone.updateSource({
-      //   "system.details.race": race,
-      //   "system.details.background": background,
-      //   "system.details.alignment": alignment
-      // });
-
       return this.document.update({
         "system.details.race": race,
         "system.details.background": background,
         "system.details.alignment": alignment,
       });
     } else if (isNPC) {
-      // this.clone.updateSource({
-      //   "system.details.environment": environment,
-      //   "system.details.alignment": alignment,
-      //   "system.details.source": source,
-      // });
-
       return this.document.update({
         "system.details.environment": environment,
         "system.details.alignment": alignment,
-        "system.details.source": source,
       });
     } else if (isVehicle) {
-      // this.clone.updateSource({
-      //   "system.traits.dimensions": dimensions,
-      //   "system.details.source": source,
-      // });
-
       return this.document.update({
         "system.traits.dimensions": dimensions,
       });
@@ -119,27 +101,11 @@ export default class Tidy5eActorOriginSummaryConfig extends Tidy5eBaseConfigShee
   /** @inheritDoc */
   activateListeners(html) {
     super.activateListeners(html);
-    // html.find(".roll-hit-points").click(this._onRollHPFormula.bind(this));
+    for (const override of this._getActorOverrides()) {
+      html.find(`input[name="${override}"],select[name="${override}"]`).each((_, el) => {
+        el.disabled = true;
+        el.dataset.tooltip = "DND5E.ActiveEffectOverrideWarning";
+      });
+    }
   }
-
-  /* -------------------------------------------- */
-
-  // /** @inheritdoc */
-  // async _onChangeInput(event) {
-  //   await super._onChangeInput(event);
-  //   const t = event.currentTarget;
-
-  //   if(t.value === "" || t.value === "0" || t.value === null || t.value === undefined) {
-  //       const rollData = this.object.getRollData();
-  //       // strange patch ...
-  //       this.object.system._source.attributes.hp.max = null;
-  //       this.object._prepareHitPoints(rollData);
-  //       t.value = this.object.system.attributes.hp.max;
-  //   }
-
-  //   // Update clone with new data & re-render
-  //   this.clone.updateSource({ [`system.attributes.${t.name}`]: t.value || null });
-  // }
-
-  /* -------------------------------------------- */
 }
